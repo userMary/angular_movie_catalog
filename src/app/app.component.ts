faebe7dabfd274c55b278b5cc4793bf4d024a9a0
userMary <mariyagmariya@yandex.ru>
2024-09-11 20:27:40 +0300

    http запрос

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchFormComponent],
   templateUrl: './app.component.html'
 })
 export class AppComponent { }
